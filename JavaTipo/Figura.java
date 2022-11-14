4c7fd8dba10da202be49d66c25e205c31118e566
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-11-14 16:46:22 -0500

    se agrego Figura sin constructor

---
+++ JavaTipo/Figura.java
@@ -0,0 +1,16 @@
+package JavaTipo;
+
+public class Figura {
+	
+	private String nombre;
+	private double area;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+}
