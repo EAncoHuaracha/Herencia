39f3397b7282a20b683a42f6983c62f97840ad0a
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-11-14 18:32:08 -0500

    se realizo la implementacion de los metodos

---
+++ Herencia_Clase_C++/Vehiculo.cc
@@ -0,0 +1,10 @@
+#include "Vehiculo.h"
+#include <iostream>
+void Vehiculo::sonido()
+{
+  std::cout << "sonido de vehiculo\n";
+}
+void Vehiculo::tipo()
+{
+  std::cout << "Soy un vehiculo: \n";
+}
